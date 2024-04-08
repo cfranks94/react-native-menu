76c92b9365f706c519b71471eff4b20d0d376373
Cord Franks <crfranks94@gmail.com>
2024-04-08 09:40:52 -0500

    cart context provider

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,8 +1,30 @@
-import { Stack } from "expo-router"
+import { Stack } from "expo-router";
+import { Link } from "expo-router";
+import { Pressable } from "react-native";
+import Colors from "@/constants/Colors";
+import { useColorScheme } from '@components/useColorScheme';
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function MenuStack() {
+    const colorScheme = useColorScheme();
+
     return (
-        <Stack>
+        <Stack screenOptions={{
+            headerRight: () => (
+                <Link href="/cart" asChild>
+                  <Pressable>
+                    {({ pressed }) => (
+                      <FontAwesome
+                        name="shopping-cart"
+                        size={25}
+                        color={Colors.light.tint}
+                        style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                      />
+                    )}
+                  </Pressable>
+                </Link>
+              ),
+        }}>
             <Stack.Screen 
                 name="index" options={{ title: 'Menu' }}
             />
